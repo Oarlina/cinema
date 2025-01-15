6ed986fc88e3b31a9bbac61557f24a0cb2cf6e7f
Rachel Marquant <marquantrachel@gmail.com>
2025-01-15 16:07:29 +0100

    Changement d'endroit

--- SQL/acteur_dans_au_moins_3_film.sql
+++ SQL/acteur_dans_au_moins_3_film.sql
@@ -1,8 +1,9 @@
-SELECT CONCAT(personne.nom, ' ', personne.prenom) AS nom_prenom, count(film.nom) AS nb_film FROM casting
+SELECT CONCAT(personne.nom, ' ', personne.prenom) AS nom_prenom, count(film.nom) AS nb_film 
+FROM casting
 
 INNER JOIN film ON casting.film_id = film.id_film
 INNER JOIN acteur ON casting.acteur_id = acteur.id_acteur
 INNER JOIN personne ON acteur.personne_id = personne.id_personne
 
-WHERE nb_film >= 3
 GROUP BY nom_prenom
+HAVING nb_film >=3
