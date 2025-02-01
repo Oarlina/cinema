2c8350585726f0a9786e1ffc4b5ebfc97d5034cc
Rachel Marquant <marquantrachel@gmail.com>
2025-02-01 20:47:32 +0100

    Page de la liste des acteurs

--- view/Actor/actorList.php
+++ view/Actor/actorList.php
@@ -4,17 +4,20 @@ if ($requete->rowCount() != 0){?>
 <p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount()?> acteurs</p>
 <button><a href="index.php?action=addActorForm">Ajouter un rôle</a></button>
 <br>
-
+<section class="corps">
 <?php
     foreach ($requete->fetchAll() as $actor) {?>   
     <div class="actorsList">
         <img class="imageActeur" src="public/img/acteurs/<?=$actor['first_name']?>.<?=$actor['forname']?>.jpg" alt="acteur image">
         <a href="index.php?action=detailActor&id=<?= $actor["id_actor"]?>"> <?= $actor["NAMES"] ?> </a>
-        <button><a href="index.php?action=deleteActor&id=<?=$actor["id_actor"] ?>">Supprimer l'acteur</a></button> 
-        <button><a href="index.php?action=deletePerson&id=<?=$actor["id_actor"] ?>">Supprimer la personne</a></button>               
+        <button class="BTNacotor"><a href="index.php?action=deleteActor&id=<?=$actor["id_actor"] ?>">Supprimer l'acteur</a></button> 
+        <button class="BTNacotor"><a href="index.php?action=deletePerson&id=<?=$actor["id_actor"] ?>">Supprimer la personne</a></button>               
     </div>    
 <?php 
 } 
+?> 
+</section>
+<?php
 }else {
     ?><p>Il n'y a aucun élément!</p> <?php
 }
